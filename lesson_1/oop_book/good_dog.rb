9697d6e5439ff9c3762935d555adc0cf3d34ad50
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-21 09:53:25 +0000

    Add getter method for name

--- lesson_1/oop_book/good_dog.rb
+++ lesson_1/oop_book/good_dog.rb
@@ -1,7 +1,21 @@
 class GoodDog
-  def initialize
-    puts "This object was initialized!"
+  def initialize(name)
+    @name = name
+  end
+
+  def get_name
+    @name
+  end
+
+  def speak
+    "#{@name} says arf!"
   end
 end
 
-sparky = GoodDog.new
+sparky = GoodDog.new("Sparky")
+puts sparky.speak
+
+fido = GoodDog.new("Fido")
+puts fido.speak
+
+puts sparky.get_name
