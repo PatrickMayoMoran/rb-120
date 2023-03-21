c008dca0dff0a1ffa4d366a7b9da7955909002d7
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-21 09:27:45 +0000

    Add example of lookup path via ancestors

--- lesson_1/good_dog.rb
+++ lesson_1/good_dog.rb
@@ -12,7 +12,13 @@ class HumanBeing
   include Speak
 end
 
-sparky = GoodDog.new
-sparky.speak('Arf!')
-bob = HumanBeing.new
-bob.speak('Hello!')
+# sparky = GoodDog.new
+# sparky.speak('Arf!')
+# bob = HumanBeing.new
+# bob.speak('Hello!')
+
+puts "---GoodDog ancestors---"
+puts GoodDog.ancestors
+puts ''
+puts "---HumanBeing ancestors ---"
+puts HumanBeing.ancestors
