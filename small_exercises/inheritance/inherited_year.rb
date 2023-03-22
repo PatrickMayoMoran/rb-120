8a64d93403cebc88fb6df7eb6d5815c2c323df93
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-22 15:15:32 +0000

    Add bed_type to Truck's instance variables

--- small_exercises/inheritance/inherited_year.rb
+++ small_exercises/inheritance/inherited_year.rb
@@ -7,9 +7,11 @@ class Vehicle
 end
 
 class Truck < Vehicle
+  attr_reader :bed_type
 
-  def initialize(year)
-    super
+  def initialize(year, bed_type)
+    super(year)
+    @bed_type = bed_type
     start_engine
   end
 
@@ -21,8 +23,6 @@ end
 class Car < Vehicle
 end
 
-truck1 = Truck.new(1994)
+truck1 = Truck.new(1994, 'Short')
 puts truck1.year
-
-car1 = Car.new(2006)
-puts car1.year
+puts truck1.bed_type
