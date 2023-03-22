052863d124b8f2ae02cbe93f7604a9b973cc0558
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-22 13:56:20 +0000

    Add walkable module for walking behaviors

--- small_exercises/1_classes_and_objects/create_the_class.rb
+++ small_exercises/1_classes_and_objects/create_the_class.rb
@@ -1,4 +1,11 @@
+module Walkable
+  def walk
+    puts "Let's go for a walk!"
+  end
+end
+
 class Cat
+  include Walkable
   attr_accessor :name
 
   def initialize(name)
@@ -13,5 +20,4 @@ end
 
 kitty = Cat.new('Sophie')
 kitty.greet
-kitty.name = 'Luna'
-kitty.greet
+kitty.walk
