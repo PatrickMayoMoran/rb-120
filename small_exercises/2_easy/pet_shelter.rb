f55419bf6ccc8adfdbd38ecd9ab0509492c196d8
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-24 09:31:34 +0000

    Add pet shelter class skeleton

---
+++ small_exercises/2_easy/pet_shelter.rb
@@ -0,0 +1,53 @@
+class Pet
+  attr_reader :animal, :name
+
+  def initialize(animal, name)
+    @animal = animal
+    @name = name
+  end
+
+end
+
+class Owner
+  attr_accessor :pets
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+end
+
+class Shelter
+
+  def adopt(owner, pet)
+  end
+
+  def print_adoptions
+  end
+
+end
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
