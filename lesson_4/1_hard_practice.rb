d0ea50d182dddd7b31db733cb0c0edf12b53fa56
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-27 14:08:11 +0000

    Add original alternative solution to hard 1 problem

---
+++ lesson_4/1_hard_practice.rb
@@ -0,0 +1,48 @@
+class Vehicle
+  attr_accessor :speed, :heading
+
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+module Wheeled
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Auto < Vehicle
+  include Wheeled
+
+  def initialize
+    super(50, 25.0)
+    @tires = [30, 30, 32, 32]
+  end
+end
+
+class Motorcycle < Vehicle
+  include Wheeled
+
+  def initialize
+    super(80, 8.0)
+    @tires = [20, 20]
+  end
+end
+
+class Catamaran < Vehicle
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    # ... code omitted ...
+  end
+end
