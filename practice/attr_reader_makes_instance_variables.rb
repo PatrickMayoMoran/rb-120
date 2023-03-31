81480246f1a59ecec072027ea2bed12cd3c8d8d5
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-31 00:24:02 +0000

    Add more examples of initializing instance variables

--- practice/attr_reader_makes_instance_variables.rb
+++ practice/attr_reader_makes_instance_variables.rb
@@ -1,8 +1,29 @@
 class Cat
   attr_reader :whiskers
+  attr_writer :whiskers
+  attr_accessor :fur
+end
+
+class Kitty
+  def initialize
+    @whiskers = nil
+    @fur = nil
+  end
 end
 
 tiny = Cat.new
-p tiny.whiskers # nil --> which makes me think yes, instance variable created
-p tiny.tail # NoMethodError, doesn't exist; what I would have expected
-# for line 6 if the zetcode distinction were true
+p tiny.inspect # No instance variables listed
+p tiny.instance_variables # Empty!
+
+p tiny.whiskers = nil
+p tiny.inspect
+p tiny.instance_variables
+
+p tiny.fur # calling a getter not only returns default nil; it also does NOT
+# initialize that instance variable
+p tiny.inspect
+p tiny.instance_variables
+
+kitty = Kitty.new
+p kitty.inspect
+p kitty.instance_variables
