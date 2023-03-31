fccb54162d700b61ed5a5f0a0780d9b32feb8eec
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-03-31 18:15:53 +0000

    Add rule descriptions for variations

--- lesson_2/2_rock_paper_scissors.rb
+++ lesson_2/2_rock_paper_scissors.rb
@@ -1,3 +1,10 @@
+def continue
+  puts "========================="
+  puts "Press enter to continue: "
+  gets
+  system 'clear'
+end
+
 class Game
   def initialize
     greet
@@ -13,6 +20,7 @@ class Game
   def greet
     puts "Welcome to Rock Paper Scissors!"
   end
+
 
   def choose_type
     Type.explain
@@ -32,37 +40,83 @@ class Game
   end
 end
 
-class Type
-  CHOICES = {1 => ::Classic, 2 => Type::RoShamBo, 3 => Type::RPSSL}
-  def initialize(moves)
-    @moves = moves
-  end
 
-  def self.explain
-    puts "There are three variations to choose from:"
-    CHOICES.each {|k,t| puts "#{k}: #{t}" }
-    choice = nil
-    loop do
-      puts "Enter 1, 2, or 3 to read a description of that variation. Enter anything else to continue."
-      choice = gets.chomp.to_i
-      break unless CHOICES.key?(choice)
-      CHOICES[choice].rules
-    end
-
-  end
-end
-
-class Classic < Type
+class Classic
   def initialize
     @moves = [:rock, :paper, :scissors]
   end
+
+  def self.rules
+  <<~HEREDOC
+  Classic Rock, Paper, Scissors works as follows:
+    1) You and your opponent choose a move: Rock, Paper, or Scissors
+    2) Moves are compared to see who wins!
+  Here are the rules:
+    Rock beats Scissors
+    Scissors beats Paper
+    Paper beats Rock
+  HEREDOC
+  end
 end
 
-class RoShamBo < Type
+class RoShamBo
+  def self.rules
+  <<~HEREDOC
+  RoShamBo works as follows:
+    1) You and your opponent choose a move: Rock, Paper, Scissors, Water, or Fire.
+    2) Moves are compared to see who wins!
+  Here are the rules:
+    Rock beats Scissors
+    Scissors beats Paper
+    Paper beats Rock
+    Fire beats everything but Water
+    Water loses to everything but Fire
+    You can choose Water as many times as you want
+    You can only use Fire once
+  HEREDOC
+  end
 end
 
-class RPSSL < Type
+class RPSSL
+  def self.rules
+    <<~HEREDOC
+    Rock, Paper, Scissors, Spock, Lizard works as follows:
+      1) You and your opponent choose a move: Rock, Paper, Scissors, Spock, or Lizard
+      2) Moves are compared to see who wins!
+    Here are the rules:
+      Rock beats Scissors and Lizard
+      Scissors beats Paper and Lizard
+      Paper beats Rock and Spock
+      Lizard beats Paper and Spock
+      Spock beats Scissors and Rock
+    HEREDOC
+  end
 end
+
+class Type
+  CHOICES = {1 => Classic, 2 => RoShamBo, 3 => RPSSL}
+
+
+  def self.explain
+    choice = nil
+    loop do
+      puts "There are three types of games: "
+      CHOICES.each {|k,t| puts "#{k}: #{t.name}" }
+      puts "Enter 1, 2, or 3 to read a description of that variation. Enter anything else to continue."
+      choice = gets.chomp.to_i
+      break unless CHOICES.key?(choice)
+
+      type = CHOICES[choice]
+      puts type.rules
+      continue
+    end
+  end
+
+  def self.choose
+  end
+
+end
+
 
 class Move
 end
