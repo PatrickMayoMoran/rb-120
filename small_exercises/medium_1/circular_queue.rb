a01249bda1e23a493c8d11c7a4510ef5b56eba69
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-01 19:47:03 +0000

    Refactor find open spot to use oldest

--- small_exercises/medium_1/circular_queue.rb
+++ small_exercises/medium_1/circular_queue.rb
@@ -52,7 +52,7 @@ class CircularQueue
     if queue.any?(nil)
       queue.index(nil)
     else
-      queue.index(oldest)
+      oldest
     end
   end
 
@@ -78,7 +78,6 @@ class CircularQueue
   private
 
   attr_reader :buffer, :queue, :tracker
-  attr_accessor :counter
 end
 
 queue = CircularQueue.new(3)
@@ -87,18 +86,18 @@ puts queue.dequeue == nil
 queue.enqueue(1)
 queue.enqueue(2)
 puts queue.dequeue == 1
-# 
-# queue.enqueue(3)
-# queue.enqueue(4)
-# puts queue.dequeue == 2
-# 
-# queue.enqueue(5)
-# queue.enqueue(6)
-# queue.enqueue(7)
-# puts queue.dequeue == 5
-# puts queue.dequeue == 6
-# puts queue.dequeue == 7
-# puts queue.dequeue == nil
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
 # 
 # queue = CircularQueue.new(4)
 # puts queue.dequeue == nil
