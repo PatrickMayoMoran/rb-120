cea5cf8885606e69f2a6120655d6491104a963e4
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-05 13:44:10 +0000

    Add messages and board display

--- lesson_5/TTT.rb
+++ lesson_5/TTT.rb
@@ -17,6 +17,30 @@ class Player
 end
 
 class TTTGame
+
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ""
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe. Goodbye!"
+  end
+
+  def display_board
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+  end
+
   def play
     display_welcome_message
     loop do
