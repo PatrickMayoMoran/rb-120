1a754b13ad99a08e64f1ae15a7558754d0bdd5ab
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-12 23:02:21 +0000

    Add rest of provided solution with output

--- small_exercises/debugging/rock_or_not_provided.rb
+++ small_exercises/debugging/rock_or_not_provided.rb
@@ -9,9 +9,24 @@ class AuthenticationError < StandardError; end
 
 # A fake search engine
 # code omitted
+class SearchEngine
+  def self.count(query, api_key)
+    unless valid?(api_key)
+      raise AuthenticationError, 'API key is not valid.'
+    end
+
+    rand(200_000)
+  end
+
+  private
+
+  def self.valid?(key)
+    key == 'LS1A'
+  end
+end
 
 module DoesItRock
-  API_KEY = 'wrong key'
+  API_KEY = 'LS1A'
 
   class NoScore; end
 
@@ -43,3 +58,7 @@ module DoesItRock
     e.message
   end
 end
+
+puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
+puts DoesItRock.find_out('Rain')        # Rain is not fun.
+puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
